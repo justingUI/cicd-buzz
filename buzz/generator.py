4f14b8bb17b3857c25f7a716c98605d96ba69b20
Justin <Justin.Greenberger@uipath.com>
2019-04-26 10:48:31 -0400

    Update generator.py

--- buzz/generator.py
+++ buzz/generator.py
@@ -4,7 +4,7 @@ import random
 buzz = ('continuous testing', 'continuous integration',
     'continuous deployment', 'continuous improvement', 'devops')
 adjectives = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end')
-adverbs = ('fantastic', 'crazy', 'volumnious', 'significantly',
+adverbs = ('fantastic', 'crazy', 'volumnious', 'horrendous',
     'seriously')
 verbs = ('accelerates', 'improves', 'enhances', 'revamps', 'boosts')
 
